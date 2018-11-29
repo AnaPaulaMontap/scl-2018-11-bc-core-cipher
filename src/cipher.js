679ab4415d5fe5018ad6b5e2979933a22194a99d
AnaPaulaMOntap <anapaula.monsalvet@gmail.com>
2018-11-29 19:03:43 -0300

    0.1v  CipherCode, sin estructura CSS, fx ok

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,40 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-  },
-  decode: () => {
-    /* Acá va tu código */
-  }
-};
+     encode: (displace,capLetter) => {
+      /* Acá va tu código */
+      let search = '';
+      for (let i=0; i<capLetter.length; i++){
+        let acsiiNumber = capLetter[i].charCodeAt();
+        if (acsiiNumber >= 65 && acsiiNumber <= 90 ) {
+          let convertedLetter=(acsiiNumber-65+displace)%26+65;
+          search += String.fromCharCode(convertedLetter);
+        }if (acsiiNumber <= 97 && acsiiNumber >= 90 ){
+          alert("Porfavor ingresa tú mensaje en MAYUSCULAS")
+          search += capLetter[i]
+        }
+      }
+        return search;
+      },
+  
+    decode: (displace,capLetter) => {
+       let search = '';
+        for (let i=0; i<capLetter.length; i++){
+          let acsiiNumber=capLetter[i].charCodeAt();
+          if (acsiiNumber >= 65 && acsiiNumber <= 90 ) {
+            let convertedLetter=(acsiiNumber-65-displace)%26+65;
+            if (convertedLetter<65){
+              convertedLetter+=26;
+            }
+            search += String.fromCharCode(convertedLetter);
+          }else if (acsiiNumber >= 97 && acsiiNumber <= 122 ) {
+            let convertedLetter=(acsiiNumber-97-displace)%26+97;
+            if (convertedLetter<97){
+              convertedLetter+=26;
+            }
+            search += String.fromCharCode(convertedLetter);
+          }else{
+            search += capLetter[i]
+          }
+          }
+          return search;
+      }
+  };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 /* Acá va tu código */
+window.onload = () => {
+    
+    const cipherData = document.getElementById("root");
+    document.getElementById("bCipher").addEventListener("click",(evento)=>{
+        evento.preventDefault();
+        let capLetter= document.getElementById("cipherText").value;
+        let displace= parseInt(document.getElementById("offset").value);
+        
+    root.innerHTML = cipher.encode(displace,capLetter)
+    });
+    const cipherDecode = document.getElementById("root2");
+    document.getElementById("dCipher").addEventListener("click",(evento)=>{
+        evento.preventDefault();
+        let capLetter= document.getElementById("cipherText").value;
+        let displace= parseInt(document.getElementById("offset").value);
+        
+    root.innerHTML = cipher.decode(displace,capLetter)
+    }); 
+
+} 
