d4cac0b98a55a2e6e33eef6c1052004d3eba1cb3
AnaPaulaMOntap <anapaula.monsalvet@gmail.com>
2018-12-07 09:10:21 -0300

    últimas modificaciones de test y botón copiar

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 /* Acá va tu código */
 window.onload = () => {
-    
-    const root = document.getElementById("root");
-    document.getElementById("bCipher").addEventListener("click",(evento)=>{
-        evento.preventDefault();
-        let capLetter= document.getElementById("cipherText").value;
-        let displace= parseInt(document.getElementById("offset").value);
-        
-    root.innerHTML = cipher.encode(displace,capLetter)
-    });
-    const root2 = document.getElementById("root");
-    document.getElementById("dCipher").addEventListener("click",(evento)=>{
-        evento.preventDefault();
-        let capLetter= document.getElementById("cipherText").value;
-        let displace= parseInt(document.getElementById("offset").value);
-        
-    root2.innerHTML = cipher.decode(displace,capLetter)
-    }); 
 
-} 
+  const root = document.getElementById("root");
+  document.getElementById("bCipher").addEventListener("click", (evento) => {
+    evento.preventDefault();
+    let capLetter = document.getElementById("cipherText").value;
+    let displace = parseInt(document.getElementById("offset").value);
+
+    root.innerHTML = cipher.encode(displace, capLetter)
+  });
+  const root2 = document.getElementById("root");
+  document.getElementById("dCipher").addEventListener("click", (evento) => {
+    evento.preventDefault();
+    let capLetter = document.getElementById("cipherText").value;
+    let displace = parseInt(document.getElementById("offset").value);
+
+    root2.innerHTML = cipher.decode(displace, capLetter)
+  });
+
+
+  let button = document.getElementById("buttonCopy"),
+    contentHolder = document.getElementById("root");
+
+  button.addEventListener("click", function () {
+
+      // We will need a range object and a selection.
+      let range = document.createRange(),
+        selection = window.getSelection();
+      selection.removeAllRanges();
+      range.selectNodeContents(contentHolder);
+      selection.addRange(range);
+      document.execCommand('copy');
+    }
+
+  );
+}
